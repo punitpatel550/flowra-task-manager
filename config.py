828e75a897009fcd7c4320ffd0ed582ea1f93d86
punit patel <153413452+punitpatel550@users.noreply.github.com>
2026-03-14 22:15:13 +0530

    database render setup

--- config.py
+++ config.py
@@ -2,9 +2,13 @@ import os
 
 class Config:
     SECRET_KEY = "supersecretkey"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:%40punit1280@localhost/task_manager"
+
+    # Render PostgreSQL connection
+    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://flowra_db_user:RN3hp87JDSRd0Oz2rnOf19mEovuTXjpk@dpg-d6qlt395pdvs73bd2gv0-a/flowra_db")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     # Twilio Credentials
-    TWILIO_ACCOUNT_SID = "AC4c3d9e6938de4f399a3657eab881bc1c"
+    TWILIO_ACCOUNT_SID = "AC4c3d9e6938de4f399a3657eab881bc1"
     TWILIO_AUTH_TOKEN = "47ba1bf93fc039d575736ef477ca25b9"
-    TWILIO_WHATSAPP_NUMBER = "+14155238886"  
+    TWILIO_WHATSAPP_NUMBER = "+14155238886"
