63fd34346c66ea571917625ce3dad3f9ae16192f
punit patel <153413452+punitpatel550@users.noreply.github.com>
2026-03-16 13:20:09 +0530

    fix config to use environment variables

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("supersecretkey")
+    SECRET_KEY = os.environ.get("SECRET_KEY", "supersecretkey")
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://flowra_db_user:RN3hp87JDSRd0Oz2rnOf19mEovuTXjpk@dpg-d6qlt395pdvs73bd2gv0-a/flowra_db") 
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    TWILIO_ACCOUNT_SID = os.environ.get( "AC4c3d9e6938de4f399a3657eab881bc1")
-    TWILIO_AUTH_TOKEN = os.environ.get( "47ba1bf93fc039d575736ef477ca25b9")
-    TWILIO_WHATSAPP_NUMBER = os.environ.get( "+14155238886")
+    TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+    TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+    TWILIO_WHATSAPP_NUMBER = os.environ.get("TWILIO_WHATSAPP_NUMBER")
