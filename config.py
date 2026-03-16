861ca8a7069f2d365ff07225aca2a05ebc47d561
punit patel <153413452+punitpatel550@users.noreply.github.com>
2026-03-16 12:13:09 +0530

    update login page and move config to environment variables

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 class Config:
-    SECRET_KEY = "supersecretkey"
+    SECRET_KEY = os.environ.get("supersecretkey")
 
-    SQLALCHEMY_DATABASE_URI = "postgresql://flowra_db_user:RN3hp87JDSRd0Oz2rnOf19mEovuTXjpk@dpg-d6qlt395pdvs73bd2gv0-a/flowra_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://flowra_db_user:RN3hp87JDSRd0Oz2rnOf19mEovuTXjpk@dpg-d6qlt395pdvs73bd2gv0-a/flowra_db") 
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    TWILIO_ACCOUNT_SID = "AC4c3d9e6938de4f399a3657eab881bc1"
-    TWILIO_AUTH_TOKEN = "47ba1bf93fc039d575736ef477ca25b9"
-    TWILIO_WHATSAPP_NUMBER = "+14155238886"
+    TWILIO_ACCOUNT_SID = os.environ.get( "AC4c3d9e6938de4f399a3657eab881bc1")
+    TWILIO_AUTH_TOKEN = os.environ.get( "47ba1bf93fc039d575736ef477ca25b9")
+    TWILIO_WHATSAPP_NUMBER = os.environ.get( "+14155238886")
